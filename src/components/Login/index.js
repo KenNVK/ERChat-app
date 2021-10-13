3dd870654891303587510859545229483f63ccaa
Khoa <nguyenvietkhoa12031993@gmail.com>
2021-10-13 09:33:38 +0900

    開発環境を構築することで

---
+++ src/components/Login/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Row, Col } from "antd";
+
+export default function Login() {
+  return (
+    <div>
+      <Row>
+        <Col span></Col>
+      </Row>
+    </div>
+  );
+}
