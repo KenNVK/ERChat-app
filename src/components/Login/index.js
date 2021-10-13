83ec3e3363187f808f01bed9440b87e2674b8ad4
Khoa <nguyenvietkhoa12031993@gmail.com>
2021-10-13 12:31:40 +0900

    login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { Row, Col } from "antd";
+import { Row, Col, Button, Typography } from "antd";
+
+const { Title } = Typography;
 
 export default function Login() {
   return (
     <>
-      <Row>
-        <Col span={8}>Login with Google</Col>
-        <Col span={8}>Login with Facebook</Col>
-        <Button type="primary">Primary Button</Button>
+      <Title style={{ textAlign: "center" }} Level={3}>
+        Login
+      </Title>
+      <Row justify="center">
+        <Col span={8}>
+          <Button type="primary" style={{ width: "100%", marginBottom: 5 }}>
+            Login with Google
+          </Button>
+
+          <Button type="primary" style={{ width: "100%" }}>
+            Login with Facebook
+          </Button>
+        </Col>
       </Row>
     </>
   );
